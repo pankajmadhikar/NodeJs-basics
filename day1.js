cdea375de8bb44d49a0683c0f686db0349445d99
pankajnirmitee <pankaj.madhikar@nirmitee.io>
2024-09-03 04:30:48 +0530

    Day 1

---
+++ day1.js
@@ -0,0 +1,53 @@
+// Problem 1
+const prompt = require("prompt-sync")();
+const alert = require("alert-node");
+
+// const age = prompt("Please Enter your Age");
+
+// if (age < 18) {
+//   alert("Ypu get a 20% discount");
+// } else if (age >= 18 && age <= 65) {
+//   alert("Normal ticket price applies");
+// } else {
+//   alert("You get a 30% seniour discount");
+// }
+
+// Problem 2
+
+// const length = prompt("Please enter area length");
+// const width = prompt("Please enter area width");
+
+// const area = length * width;
+
+// console.log("length", length);
+// console.log("width", width);
+// console.log("area", area);
+// alert("Your area in sq.ft = ", area);
+
+// Problem 3
+
+// const products = [
+//   { name: "product1", price: 100, inStock: true },
+//   { name: "product2", price: 200, inStock: false },
+//   { name: "product3", price: 300, inStock: true },
+// ];
+
+// console.log(products);
+
+// Problem 4
+
+// const guestList = ["Pankaj", "Mohit", "Rushi", "Ajay", "Vishal"];
+
+// const guestName = prompt("Enter your name");
+
+// const isAvailable = guestList.find((guest) => {
+//   return guest === guestName ? true : false;
+// });
+// console.log(isAvailable);
+// if (!!isAvailable) {
+//   console.log("Welcome to the party ", guestName);
+// } else {
+//   console.log("You are not in the guest list");
+// }
+
+// Problem 5
