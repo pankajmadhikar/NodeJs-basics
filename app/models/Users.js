f63a87ef9146d59d226263aa30488caf43548a6d
pankajmadhikar <pankajmadhikar308@gmail.com>
2026-01-08 18:58:14 +0530

    Add bcryptjs and jsonwebtoken dependencies; format UserSchema in Users.js

--- app/models/Users.js
+++ app/models/Users.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    password:{type:String, required:true},
-    age:{type:Number}
-},{timestamps:true})
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    age: { type: Number },
+  },
+  { timestamps: true }
+);
 
-export const UserModel = mongoose.model("User", UserSchema)
+export const UserModel = mongoose.model("User", UserSchema);
